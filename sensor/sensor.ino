5ec3eabaa02eade9900a3f39d1cf24e2bf71be58
Ruben <megarex333@gmail.com>
2021-04-14 18:34:58 +0200

    Sensor code

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -2,106 +2,46 @@
 #include "secrets.h"
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
+#include <ArduinoJson.h>
 
 // Pinout
-#define BUTTON 16
-#define AE_LED D3
-#define CNT_DESCRIPTOR_LED D4
-#define CNT_DATA_LED D5
-#define SEND_LED D6
-
-// LCD
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define SETUP_LED 2
+#define SEND_LED 16
 
 // CSE
-const String address = String(CSE_SCHEMA) + "://" + String(CSE_ADDRESS) + ":" + String(CSE_PORT);
+const String CSE_URL = String(CSE_SCHEMA) + "://" + String(CSE_ADDRESS) + ":" + String(CSE_PORT) + "/";
 
-// AE
-const String AE_ENDPOINT = String(CSE_ENDPOINT) + "/" + String(RESOURCE_NAME);
-const String ORIGINATOR = String(CSE_ORIGINATOR) + String(RESOURCE_NAME);
-
-// Vars
-uint32_t delayStart = 0;
-uint8_t buttonPrev;
-bool menu = 1;
+// CNT
+const String SENSORS_ENDPOINT = CSE_URL + "cse-in/SmartApp/Sensors";
+bool isSubscribed = false;
 
 void setup()
 {
   Serial.begin(115200);
-  lcd.init();
-  lcd.backlight();
-  pinMode(BUTTON, INPUT);
-  pinMode(AE_LED, OUTPUT);
-  pinMode(CNT_DATA_LED, OUTPUT);
-  pinMode(CNT_DESCRIPTOR_LED, OUTPUT);
+  pinMode(SETUP_LED, OUTPUT);
   pinMode(SEND_LED, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(A0, INPUT);
 
   if (connectWiFi())
   {
-    if (handler(registerAE(), "AE", "REGISTRATION"))
+    if (handler(registerCnt("", String(RESOURCE_NAME)), "SENSOR CNT", "CREATION"))
     {
-      digitalWrite(AE_LED, HIGH);
-      uint16_t mni = 1;
-      handler(registerCnt("DESCRIPTOR", String(mni)), "DESCRIPTOR", "CREATION");
-      digitalWrite(CNT_DESCRIPTOR_LED, HIGH);
-      handler(registerCnt("DATA", String(MAX_CIN)), "DATA", "CREATION");
-      digitalWrite(CNT_DATA_LED, HIGH);
-      String positionData = "[" + String(LONGITUDE, 6) + "," + String(LATITUDE, 6) + "]";
-      handler(postData(AE_ENDPOINT + "/DESCRIPTOR", positionData), "POSITION",
-              "UPDATE");
+      // Create LOCATION and instantiate container
+      handler(registerCnt("/" + String(RESOURCE_NAME), "LOCATION"), "LOCATION CNT", "CREATION");
+      handler(registerCnt("/" + String(RESOURCE_NAME), "DATA"), "DATA CNT", "CREATION");
+      handler(postLocation(), "POSITION", "UPDATE");
     }
-    else
-    {
-      digitalWrite(AE_LED, HIGH);
-      digitalWrite(CNT_DATA_LED, HIGH);
-      digitalWrite(CNT_DESCRIPTOR_LED, HIGH);
-    }
-    pinMode(BUTTON, INPUT_PULLUP);
-    buttonPrev = digitalRead(BUTTON);
   }
-  buttonPrev = digitalRead(BUTTON);
+  digitalWrite(SETUP_LED, LOW);
+  Serial.println("SETUP DONE");
 }
 
 void loop()
 {
-  if ((delayStart == 0) || ((millis() - delayStart > REQUEST_PERIOD)))
-  {
-    delayStart = millis();
-    digitalWrite(SEND_LED, HIGH);
-    handler(postData(AE_ENDPOINT + "/DATA", readData()), "DATA", "UPDATE");
-    digitalWrite(SEND_LED, LOW);
-  }
-  if (digitalRead(BUTTON) == HIGH and buttonPrev == LOW)
-  {
-    updateView();
-  }
-  buttonPrev = digitalRead(BUTTON);
-}
-
-void printLCD(String fl, String sl = "")
-{
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(fl);
-  if (sl.length() > 0)
-    ;
-  {
-    lcd.setCursor(0, 1);
-    lcd.print(sl);
-  }
-}
-
-String readData()
-{
-  String data;
-  data += "{\\\"co2\\\":" + String(random(100, 2500)) + ",";
-  data += "\\\"o3\\\":" + String(random(5, 125)) + ",";
-  data += "\\\"no2\\\":" + String(random(5, 76)) + ",";
-  data += "\\\"so2\\\":" + String(random(10, 250)) + "}";
-  return data;
+  digitalWrite(SEND_LED, LOW);
+  handler(postReading(), "DATA", "UPDATE");
+  digitalWrite(SEND_LED, HIGH);
+  delay(REPORT_FREQ);
 }
 
 // Setup Functions
@@ -112,83 +52,102 @@ uint8_t connectWiFi()
   Serial.print("Connecting to: ");
   Serial.println(SECRET_SSID);
 
-  printLCD("CONNECTING");
-
   while (WiFi.status() != WL_CONNECTED and i < MAX_ATTEMPTS)
   {
     Serial.print(".");
     delay(500);
-    lcd.setCursor(i, 1);
-    lcd.print("*");
     i++;
   }
   if (i < MAX_ATTEMPTS)
   {
     Serial.println("\nConnection established!");
-    printLCD("CONNECTED TO", SECRET_SSID);
+    Serial.print("Local IP: ");
+    Serial.println(WiFi.localIP());
     return 1;
   }
   else
   {
     Serial.println("\nConnection to WiFi failed.");
-    printLCD("ERROR");
     return 0;
   }
 }
 
 // CSE Functions
-int16_t handler(int16_t responseCode, String resource, String action)
+int16_t handler(int16_t responseCode, const String &resource, const String &action)
 {
   if (responseCode == 201)
   {
     Serial.println(resource + " " + action + " SUCCESS\n");
-    printLCD(resource + " " + action, "SUCCESS");
     return 1;
   }
   else
   {
     Serial.print(resource + " " + action);
     Serial.printf(" ERROR: CODE %d\n\n", responseCode);
-    printLCD(resource + " " + action, "ERROR");
     return 0;
   }
 }
 
-uint16_t registerAE()
+uint16_t registerCnt(const String &endpoint, const String &rn)
 {
-  String payload = "{\"m2m:ae\":{\"rn\":\"" + String(RESOURCE_NAME) +
-                   String("\",\"api\":\"N01.ROTehc.com.") +
-                   String(RESOURCE_NAME) +
-                   String("\",\"srv\":[\"3\"],\"rr\":false}}");
-
-  return postToCse(CSE_ENDPOINT, payload, 2);
+  StaticJsonDocument<128> payload;
+  payload["m2m:cnt"]["mni"] = 1;
+  payload["m2m:cnt"]["rn"] = rn;
+  String payloadString;
+  serializeJson(payload, payloadString);
+  return postToCse(endpoint, payloadString, 3);
 }
 
-uint16_t registerCnt(String rn, String mni)
+uint16_t postLocation()
 {
-  String payload = "{\"m2m:cnt\":{\"mni\":" + mni + ",\"rn\":\"" + rn + "\"}}";
-  return postToCse(AE_ENDPOINT, payload, 3);
+  StaticJsonDocument<192> payload;
+  payload["m2m:cin"]["cnf"] = "application/json:0";
+  StaticJsonDocument<92> loc;
+  loc["lat"] = String(LATITUDE, 6);
+  loc["lon"] = String(LONGITUDE, 6);
+  String locString;
+  serializeJson(loc, locString);
+  payload["m2m:cin"]["con"] = locString;
+  JsonArray lbl = payload["m2m:cin"].createNestedArray("lbl");
+  lbl.add("LOCATION");
+  String payloadString;
+  serializeJson(payload, payloadString);
+  return postToCse("/" + String(RESOURCE_NAME) + "/LOCATION", payloadString, 4);
 }
 
-uint16_t postData(String endpoint, String data)
+uint16_t postReading()
 {
-  String payload =
-      "{\"m2m:cin\":{\"cnf\":\"application/json\",\"con\":\"" + data + "\"}}";
-  return postToCse(endpoint, payload, 4);
+  int co2 = 1500 / 1024 * analogRead(A0) + 200;
+  Serial.println("CO2: " + co2);
+  StaticJsonDocument<192> payload;
+  payload["m2m:cin"]["cnf"] = "application/json:0";
+  StaticJsonDocument<92> con;
+  con["co2"] = co2;
+  con["o3"] = random(20, 65);
+  con["no2"] = random(15, 50);
+  con["so2"] = random(15, 50);
+  String conString;
+  serializeJson(con, conString);
+  payload["m2m:cin"]["con"] = conString;
+  JsonArray lbl = payload["m2m:cin"].createNestedArray("lbl");
+  lbl.add("DATA");
+  String payloadString;
+  serializeJson(payload, payloadString);
+  return postToCse("/" + String(RESOURCE_NAME) + "/DATA", payloadString, 4);
 }
 
-uint32_t postToCse(String endpoint, String payload, uint8_t ty)
+uint32_t postToCse(const String &endpoint, const String &payload, uint8_t ty)
 {
   HTTPClient client;
-  String url = address + endpoint;
+  String url = SENSORS_ENDPOINT + endpoint;
   if (!client.begin(url))
   {
     Serial.println("Connection to host failed");
     return 0;
   }
-  client.addHeader("X-M2M-Origin", ORIGINATOR);
+  client.addHeader("X-M2M-Origin", "SmartApp");
   client.addHeader("X-M2M-RVI", String(CSE_RELEASE));
-  client.addHeader("X-M2M-RI", "123456");
+  client.addHeader("X-M2M-RI", String(random(10000, 99999)));
   client.addHeader("Content-Type",
                    "application/vnd.onem2m-res+json; ty=" + String(ty));
   client.addHeader("Content-Length", String(payload.length()));
@@ -196,26 +155,10 @@ uint32_t postToCse(String endpoint, String payload, uint8_t ty)
 
   // Send POST
   int16_t statusCode = client.POST(payload);
-  Serial.println("POST sent to: " + address + endpoint + " with status " + statusCode);
-  Serial.print(payload);
-  Serial.print("\n\nResponse: ");
+  Serial.println("POST sent to: " + url + " with status " + statusCode);
+  Serial.println(payload);
+  Serial.println("Response:");
   Serial.println(client.getString());
-  Serial.println();
   client.end();
   return statusCode;
-}
-
-void updateView()
-{
-  if (menu)
-  {
-    printLCD("CONNECTED TO", SECRET_SSID);
-  }
-  else
-  {
-    printLCD(String("CO2:") + random(200, 2400) + " NO2:" + random(20, 140),
-             String("SO2:") + random(40, 250) + "  O3: " + random(20, 90));
-  }
-  menu = !menu;
-  delay(100);
 }
